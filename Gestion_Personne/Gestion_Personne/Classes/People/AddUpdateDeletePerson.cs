b946edbab3a8252e96e17a756995ef7a6e94698f
james-mathe <kambalemathejacques3o@gmail.com>
2025-04-10 17:19:50 +0200

    adding peaple,phone and address classes

---
+++ Gestion_Personne/Gestion_Personne/Classes/People/AddUpdateDeletePerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Gestion_Personne.Classes.People
+{
+    class AddUpdateDeletePerson
+    {
+        private Config db;
+        private SqlConnection sqlcon;
+        private MySqlConnection mycon;
+        private SqlCommand sqlcmd;
+        private MySqlCommand mycmd;
+    }
+}
