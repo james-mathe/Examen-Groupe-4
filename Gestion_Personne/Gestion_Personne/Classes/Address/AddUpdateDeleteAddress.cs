9a773808561fbee8a5a0dcc6c064ff48ec393b1f
james-mathe <kambalemathejacques3o@gmail.com>
2025-04-10 15:36:47 +0200

    add address class CRUD

---
+++ Gestion_Personne/Gestion_Personne/Classes/Address/AddUpdateDeleteAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Gestion_Personne.Classes.Address
+{
+    class AddUpdateDeleteAddress
+    {
+        private Config db;
+        private SqlConnection sqlcon;
+        private MySqlConnection mycon;
+        private SqlCommand sqlcmd;
+        private MySqlCommand mycmd;
+
+        public bool addAddress(int idP,String quarter,String avenue,String com,String city,String Contry)
+        {
+            return true;
+
+        }
+    }
+}
