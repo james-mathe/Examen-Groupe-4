550e7b20ee0d20d460aa52a016d4b833cc26c4a5
james-mathe <kambalemathejacques3o@gmail.com>
2025-04-13 22:46:24 +0200

    creating an Update procedure for person and test it

--- db/Sql Server/procedures.sql
+++ db/Sql Server/procedures.sql
@@ -3,13 +3,24 @@ use g_personne
 go
 -- procedure for inserting personne
 
-create proc addPerson @nom varchar(250),@pnom varchar(250),@prnom varchar(250),@sex char(1)
+create or alter proc addPerson @nom varchar(250),@pnom varchar(250),@prnom varchar(250),@sex char(1)
 as
 begin
-	insert into personne()
+	insert into personne(nom,postnom,prenom,sexe) values(@nom,@pnom,@prnom,@sex)
 end
+go
 -- procedure for updating personne
 
+create or alter proc UpdatePerson @id bigint,@nom varchar(250),@pnom varchar(250),@prnom varchar(250),@sex char(1)
+as
+begin
+	update personne set nom=@nom,postnom=@pnom,prenom=@prnom,sexe=@sex where idP=@id
+end
 -- procedure for searching personne
 
 -- procedure for deleting personne
+
+
+exec UpdatePerson 2,'Kahindo','mathe','yvone','F'
+
+select * from personne
