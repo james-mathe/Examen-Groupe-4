eb94891b375feed950b0b267d7bbef116f9aae41
james-mathe <kambalemathejacques3o@gmail.com>
2025-04-14 11:29:57 +0200

    Updating the procedure file

--- db/Sql Server/procedures.sql
+++ db/Sql Server/procedures.sql
@@ -1,6 +1,9 @@
 
 use g_personne
 go
+-- ==================== START PERSON PROCEDURE==========================================
+
+
 -- procedure for inserting personne
 
 create or alter proc AddPerson @nom varchar(250),@pnom varchar(250),@prnom varchar(250),@sex char(1)
@@ -9,6 +12,7 @@ begin
 	insert into personne(nom,postnom,prenom,sexe) values(@nom,@pnom,@prnom,@sex)
 end
 go
+
 -- procedure for updating personne
 
 create or alter proc UpdatePerson @id bigint,@nom varchar(250),@pnom varchar(250),@prnom varchar(250),@sex char(1)
@@ -17,6 +21,7 @@ begin
 	update personne set nom=@nom,postnom=@pnom,prenom=@prnom,sexe=@sex where idP=@id
 end
 go
+
 -- procedure for searching personne
 
 create or alter proc SearchPerson @nom varchar(250),@pnom varchar(250),@prnom varchar(250),@sex char(1)
@@ -25,6 +30,7 @@ begin
 	select * from personne  where nom like '%'+@nom+'%' or postnom like '%'+@pnom+'%' or prenom like '%'+@prnom+'%' or sexe like '%'+@sex+'%'
 end
 go
+
 -- procedure for deleting personne
 
 create or alter proc DeletePerson @id bigint
@@ -32,3 +38,51 @@ as
 begin
 	delete from personne  where idP=@id
 end
+go
+
+-- ==================== END PERSON PROCEDURE==========================================
+
+
+-- ==================== START USER PROCEDURE==========================================
+
+
+-- procedure for inserting users
+
+-- procedure for updating users
+
+-- procedure for searching users
+
+-- procedure for deleting users
+
+
+-- ==================== END USER PROCEDURE==========================================
+
+
+-- ==================== START TELEPHONE PROCEDURE==========================================
+
+
+-- procedure for inserting telephone
+
+-- procedure for updating telephone
+
+-- procedure for searching telephone
+
+-- procedure for deleting telephone
+
+
+-- ==================== END TELEPHONE PROCEDURE==========================================
+
+
+-- ==================== START ADDRESS PROCEDURE==========================================
+
+
+-- procedure for inserting adresse
+
+-- procedure for updating adresse
+
+-- procedure for searching adresse
+
+-- procedure for deleting adresse
+
+
+-- ==================== END ADDRESS PROCEDURE==========================================
